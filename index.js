dce908b2822565b52cf82cd160a05bb3ca903b79
+ <+>
2022-06-07 17:03:04 +0300

    commit tree 93749276bd8ce5a51fb42589f7f14f24f860ee05

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(distance) {
+  if (distance > 42) {
+    return distance - 42;
+  } else {
+    return 42 - distance;
+  }
+}
+function distanceFromHqInFeet(feet) {
+  if (feet < 42) {
+    return feet - 42;
+  } else {
+    return 42 - feet;
+  }
+}
+function distanceTravelledInFeet(start, destination) {
+  if (start < destination) {
+    return (destination - start) * 264;
+  } else {
+    return (start - destination) * 264;
+  }
+}
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  if (distance <= 400) {
+    return "gives customers a free sample";
+  } else if (distance > 400 && distance <= 2000) {
+    return 0.02 * (distance - 400);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
